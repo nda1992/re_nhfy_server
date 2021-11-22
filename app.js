f9d84e2917886abf62255cf05b9fb4aa9a5a2e3e
nongdingan <nda_1992@163.com>
2021-11-22 09:14:11 +0800

    数据图表的展示

--- app.js
+++ app.js
@@ -14,19 +14,22 @@ const websiteRouter = require('./routes/website/website')
 const getnewsRouter = require('./routes/website/getnews')
 const reportmakeRouter = require('./routes/reportmake/reportmake')
 const employeeRouter = require('./routes/hospital/employee')
+const inpatientRouter = require('./routes/report/inpatient')
+const outpatientRouter = require('./routes/report/outpatient')
 const websitemanagerRouter = require('./routes/websitemanager/manager')
+
 // 微信的路由
 const wechatRouter = require('./routes/wechat/wechat')
-// const specialQueryRouter = require('./routes/reportmake/specialQuery')
-// // 查询主题的相关路由
-// const revenueRouter = require('./routes/QueryTheam/revenue/revenue')
-// const operationRouter = require('./routes/QueryTheam/operation/operation')
-// const rankRouter = require('./routes/QueryTheam/rank/rank')
-// const othersRouter = require('./routes/QueryTheam/others/others')
-// const basicRouter = require('./routes/QueryTheam/basic/basic')
-// const bydeptRouter = require('./routes/QueryTheam/bydept/bydept')
+const specialQueryRouter = require('./routes/reportmake/specialQuery')
+// 查询主题的相关路由
+const revenueRouter = require('./routes/QueryTheam/revenue/revenue')
+const operationRouter = require('./routes/QueryTheam/operation/operation')
+const rankRouter = require('./routes/QueryTheam/rank/rank')
+const othersRouter = require('./routes/QueryTheam/others/others')
+const basicRouter = require('./routes/QueryTheam/basic/basic')
+const bydeptRouter = require('./routes/QueryTheam/bydept/bydept')
 // // 数据展示路由
-// const visualizeRouter = require('./routes/Visualize/visualize')
+const visualizeRouter = require('./routes/Visualize/visualize')
 const app = express();
 
 const { varifyToken } = require('./utils/token')
@@ -62,12 +65,12 @@ app.use((req, res, next) => {
             break
         }
     }
-    let whileUrl = ['/users/login', '/position/jobSeekerRegister', '/users/updatePassword']
+    const whileUrl = ['/users/login', '/position/jobSeekerRegister', '/users/updatePassword']
     if (whileUrl.indexOf(url) >= 0 || trueUrl !== -1) {
         return next()
     }
-    let token = req.headers.token
-    let result = varifyToken(token)
+    const token = req.headers.token
+    const result = varifyToken(token)
     if (!result) {
         res.json({ code: 201, msg: 'token验证失败' })
     } else {
@@ -87,16 +90,19 @@ app.use('/website/news', getnewsRouter)
 app.use('/reportmake', reportmakeRouter)
 app.use('/employee', employeeRouter)
 app.use('/websitemanager', websitemanagerRouter)
+app.use('/report/inpatient', inpatientRouter)
+app.use('/report/outpatient', outpatientRouter)
 app.use('/wechat', wechatRouter)
+
     // 主题查询的路由
-// app.use('/reportmake/specialQuery', specialQueryRouter)
-// app.use('/QueryTheam/operation', operationRouter)
-// app.use('/QueryTheam/revenue', revenueRouter)
-// app.use('/QueryTheam/rank', rankRouter)
-// app.use('/QueryTheam/others', othersRouter)
-// app.use('/QueryTheam/basic', basicRouter)
-// app.use('/QueryTheam/bydept', bydeptRouter)
-// app.use('/visualize', visualizeRouter)
+app.use('/reportmake/specialQuery', specialQueryRouter)
+app.use('/QueryTheam/operation', operationRouter)
+app.use('/QueryTheam/revenue', revenueRouter)
+app.use('/QueryTheam/rank', rankRouter)
+app.use('/QueryTheam/others', othersRouter)
+app.use('/QueryTheam/basic', basicRouter)
+app.use('/QueryTheam/bydept', bydeptRouter)
+app.use('/visualize', visualizeRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
